53544882d5167cc9b0dd188fa4db4d1aca7b445f
tsriramanamanjarry <143001940+tsriramanamanjarry@users.noreply.github.com>
2024-09-26 11:38:31 +0530

    Create defaultvalue.java

---
+++ defaultvalue.java
@@ -0,0 +1,18 @@
+public class Default{
+static int a;
+static byte b;
+static short c;
+static long s;
+static float e;
+static String f;
+static double g;
+public static void main(String[]args){
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(s);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+}
+}
